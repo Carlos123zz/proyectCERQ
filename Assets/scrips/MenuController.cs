1341c314d327b82f8909870771ee473b8ecbbf30
carlos <carlos.reyes@uteca.edu.mx>
2026-09-17 20:29:36 -0600

    Alex

--- Assets/scrips/MenuController.cs
+++ Assets/scrips/MenuController.cs
@@ -1,35 +1,53 @@
 using UnityEngine;
+
 using UnityEngine.Events;
-using UnityEgine.SceneManagement;
+
+using UnityEngine.SceneManagement;
+
 using System.Collections;
 
 public class MenuController : MonoBehaviour
+
 {
+
     [SerializeField]
 
     private UnityEvent onSceneStarted;
+
     [SerializeField]
 
     private Animator fade;
+
     [SerializeField]
 
     private string fadeOutAnimationName = "FadeOut";
 
-    private void start();
-    {
-             onSceneStarted?.Invoke();
-}
+    private void Start()
 
-{
-    public void GoToSceneWithFade (string sceneName)
-{
-    StarCoroutine(LoadSceneWithFade(sceneName));
-}
-           private IEnumerator LoadSceneWithFade(string sceneName)
-{
-    GoToSceneWithFade().play(fadeOutAnimationName, 0, 0f)
-        yield return new WaitForSeconds(1f);
-    SceneManager.LoadScene(sceneName)   ;
+    {
+
+        onSceneStarted?.Invoke();
 
     }
+
+    public void GoToSceneWithFade(string sceneName)
+
+    {
+
+        StartCoroutine(LoadSceneWithFade(sceneName));
+
+    }
+
+    private IEnumerator LoadSceneWithFade(string sceneName)
+
+    {
+
+        fade.Play(fadeOutAnimationName, 0, 0f);
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(sceneName);
+
+    }
+
 }
