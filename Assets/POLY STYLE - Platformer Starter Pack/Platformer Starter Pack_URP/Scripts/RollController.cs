fb3a7f166fc5bc19c0fd54de6ce1e1996c302670
carlos <carlos.reyes@uteca.edu.mx>
2026-09-24 20:34:09 -0600

    coin

--- Assets/POLY STYLE - Platformer Starter Pack/Platformer Starter Pack_URP/Scripts/RollController.cs
+++ Assets/POLY STYLE - Platformer Starter Pack/Platformer Starter Pack_URP/Scripts/RollController.cs
@@ -6,18 +6,33 @@ public class RollController : MonoBehaviour
     [SerializeField]
     private InputController inputController;
     [SerializeField]
-
     private UnityEvent onRoll;
+    [SerializeField]
+    private float rollDuration = 1f;
+    [SerializeField]
+    private GameObject rollEffectPrefab;
+    [SerializeField]
+    private float effectOffsetY = 0.5f;
+    private bool isRolling = false;
 
     private void Update()
     {
-        if (inputController.Roll)
+        if (!isRolling && inputController.Roll)
         {
             Roll();
         }
     }
     private void Roll()
     {
+        PoolManager.Instance.GetObject(rollEffectPrefab, transform.position+(Vector3.up*effectOffsetY));
+        isRolling = true;
         onRoll?.Invoke();
+        Invoke(nameof(EndRoll), rollDuration);
+
+    }
+
+    private void EndRoll()
+    {
+    isRolling = false;
     }
 }
